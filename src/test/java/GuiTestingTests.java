8f24c4834b9881050f3f9233b2e4165eb0531262
kejsa <Fingal95>
2019-10-14 16:45:46 +0200

    Initial

---
+++ src/test/java/GuiTestingTests.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+import se.claremont.eyeautomatesupport.EyeAutomateDriver;
+import se.claremont.taf.core.logging.GenericJavaObjectToHtml;
+import se.claremont.taf.core.testcase.TestCase;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GuiTestingTests {
+
+    @Test
+    public void initializationTest() throws IOException {
+        TestCase testCase = new TestCase();
+        EyeAutomateDriver driver = new EyeAutomateDriver(testCase);
+        Files.write(Paths.get("C:\\temp\\test.html"), GenericJavaObjectToHtml.toHtmlPage(driver).getBytes());
+        Assert.assertNotNull(driver.scriptRunner);
+    }
+}
